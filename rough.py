aaa512cade5fdb31fc72f1d2b6b60e20f82e9cf5
sohamkekre <sohamkekresoham@gmail.com>
2024-06-26 20:36:27 +0530

    26/06/24

--- rough.py
+++ rough.py
@@ -1,9 +1,2 @@
-n = int(input())
-otp = 0
-for i in range(n):
-    operations = str(input())
-    if '--' in operations :
-        otp -= 1
-    else :
-        otp += 1
-print(otp)
+l = list(map(int,input("Enter the n and k: ").split(" ")))
+
