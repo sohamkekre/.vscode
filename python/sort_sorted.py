3ae9a62517f33785cf5ba05f8d9ae5082eb8a4db
sohamkekre <sohamkekresoham@gmail.com>
2024-08-03 20:24:43 +0530

    03/08/2024

---
+++ python/sort_sorted.py
@@ -0,0 +1,5 @@
+# arr_sorted = sorted(list(map(int,input().split())))
+# sorted function can be used when we need to sort the input while the input operation is going and the sort function is used when we have taken the input and then we need to sort the arr
+# arr_sort = [5,4,6,7,3,2,1]
+# arr_sort.sort()
+# print(arr_sort)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,3 @@
-pneumonoultramicroscopicsilicovolcanoconiosis
+list = [1,2,3,4,5,6]
+list.append(7)
+print(list)
